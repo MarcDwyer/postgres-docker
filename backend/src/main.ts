c2387dc85bf552399478dafd8c2a49a2c5bed109
Marc Dwyer <mdwyer@Locals-iMac.ihmc.us>
2020-01-22 16:21:35 -0600

    yeet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,26 +8,30 @@ const user = "root",
 
 // dburl = `mongodb://${DBUSER}:${DBPASS}@localhost:4200`,
 
+const queries = {
+  dropIf: "DROP TABLE IF EXISTS account"
+};
 async function main() {
   try {
     const client = new Client({
       connectionString: `postgres://${user}:${password}@localhost:5432/${dbName}`
     });
     await client.connect();
+    await client.query(queries.dropIf);
     await client.query(`
     CREATE TABLE account(
        user_id serial PRIMARY KEY,
-       username VARCHAR (50) UNIQUE NOT NULL,
-       password VARCHAR (50) NOT NULL,
-       email VARCHAR (355) UNIQUE NOT NULL,
-       number INTEGER(32) NULL
+       username varchar(50) UNIQUE NOT NULL,
+       password varchar(50) NOT NULL,
+       email varchar(355) UNIQUE NOT NULL,
+       phonenumber int
     );`);
 
-    // const res = await client.query(`
-    // INSERT INTO account (username, password, email)
-    //     VALUES ('marc', 'test2', 'envdia@gmail.com');
-    // `);
-    //    console.log(res);
+    const res = await client.query(`
+    INSERT INTO account (username, password, email)
+        VALUES ('marc', 'test2', 'envdia@gmail.com');
+    `);
+    console.log(res);
   } catch (err) {
     console.error(err);
   }
