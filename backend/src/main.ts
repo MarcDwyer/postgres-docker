67bd5bfa93ef4f623b8c38b5218c1a3bdeb67f6a
Marc Dwyer <mdwyer@Locals-iMac.ihmc.us>
2020-01-22 15:04:13 -0600

    we out here trying

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,12 +11,23 @@ const user = "root",
 async function main() {
   try {
     const client = new Client({
-      user: "root",
-      password: "yeet",
-      database: "mydb",
       connectionString: `postgres://${user}:${password}@localhost:5432/${dbName}`
     });
     await client.connect();
+    await client.query(`
+    CREATE TABLE account(
+       user_id serial PRIMARY KEY,
+       username VARCHAR (50) UNIQUE NOT NULL,
+       password VARCHAR (50) NOT NULL,
+       email VARCHAR (355) UNIQUE NOT NULL,
+       number INTEGER(32) NULL
+    );`);
+
+    // const res = await client.query(`
+    // INSERT INTO account (username, password, email)
+    //     VALUES ('marc', 'test2', 'envdia@gmail.com');
+    // `);
+    //    console.log(res);
   } catch (err) {
     console.error(err);
   }
